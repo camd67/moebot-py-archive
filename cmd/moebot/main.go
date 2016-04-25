a2e35b496c8874858fb4d8ceb188aef3f11e9f1d
CamD67 <cameron.doane@gmail.com>
2016-04-24 20:09:00 -0700

    start up heroku

---
+++ cmd/moebot/main.go
@@ -0,0 +1,55 @@
+package main
+
+import
+//"net/http"
+//"os"
+
+(
+	"fmt"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+//"github.com/gin-gonic/gin"
+
+func main() {
+	/*
+		port := os.Getenv("PORT")
+
+		if port == "" {
+			log.Fatal("$PORT must be set")
+		}
+
+		router := gin.New()
+		router.Use(gin.Logger())
+		router.LoadHTMLGlob("templates/*.tmpl.html")
+		router.Static("/static", "static")
+
+		router.GET("/", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		})
+
+		router.Run(":" + port)
+	*/
+	discord, err := discordgo.New("MTcyNDk1ODI2MjY0OTE1OTY4.Cf8JQg.b5nU5Y6qOUYQwejYPcq0shaGHdk")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	discord.AddHandler(messageCreate)
+
+	err = discord.Open()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("bot up and running!")
+
+	fmt.Printf("bot is running, press CTRL+C to exit")
+	<-make(chan struct{})
+	return
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	fmt.Printf("%20s %20s %20s > %s\n", m.ChannelID, time.Now().Format(time.Stamp), m.Author.Username, m.Content)
+}
