c0ec5bfe967dc836278ecd86a6a7931cba065e2d
CamD67 <cameron.doane@gmail.com>
2016-04-25 17:33:11 -0700

    added env token

--- cmd/moebot/main.go
+++ cmd/moebot/main.go
@@ -2,39 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 	"os"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-
-    discordSetup()
-
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
-
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
-	router.Run(":" + port)
-}
-
-func discordSetup()  {
-	discord, err := discordgo.New(os.Args[1])
+	discord, err := discordgo.New(os.Getenv("DISCORD_TOKEN"))
 	if err != nil {
 		fmt.Println(err)
 		return
